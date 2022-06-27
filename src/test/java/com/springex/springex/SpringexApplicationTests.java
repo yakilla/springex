f5626195202f6164a1fc24359997f87dfc02655b
yenik <nikandrov15@gmail.com>
2022-06-27 19:09:06 +0300

    Initial commit

---
+++ src/test/java/com/springex/springex/SpringexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springex.springex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
